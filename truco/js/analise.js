668bd49617a2feb229949b24d3f562fe32945daa
mariapassos23 <passosbarbosademoraesmaria@gmail.com>
2023-08-15 20:32:22 -0400

    git analisis
    criei uma função que valida o botão e ajustei o html com span para aparecer a pontuação

--- truco/js/analise.js
+++ truco/js/analise.js
@@ -1,5 +1,12 @@
 let team1point = 0;
 let team2point = 0;
-function updatepoint(params) {
+function updatepoint(team,points) {
+    if (team ==='team1'){
+        team1point += points;
+        document.getElementById('team1point')
+    } else if (team === 'team2') {
+        team2point += points;
+        document.getElementById('team2point').textContent = team2point;
+      }
     
 }
